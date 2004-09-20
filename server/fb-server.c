dd072a67250a60303101ef9868793fe2f253f66f
gc <gc>
2004-09-20 18:07:32 +0000

    GPL

--- server/fb-server.c
+++ server/fb-server.c
@@ -1,21 +1,23 @@
-/*
+/*******************************************************************************
+ *
  * Copyright (c) 2004 Guillaume Cottenceau
  *
- */
-
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <sys/types.h>
-#include <errno.h>
-
-#include <glib.h>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ ******************************************************************************/
 
 #include "net.h"
-
 
 int main(int argc, char **argv)
 {
