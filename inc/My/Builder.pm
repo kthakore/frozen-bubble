d75a3ba880f0125f51d3fef0ee347c2e5e88694b
Lars Dɪᴇᴄᴋᴏᴡ 迪拉斯 <daxim@cpan.org>
2010-04-19 17:42:39 +0200

    server target

--- inc/My/Builder.pm
+++ inc/My/Builder.pm
@@ -2,11 +2,14 @@ package My::Builder;
 use 5.008;
 use strict;
 use warnings FATAL => 'all';
-use autodie qw(:all);
+use ExtUtils::CBuilder qw();
 use File::Basename qw(fileparse);
+use File::Copy qw(move);
+use File::Slurp qw(read_file write_file);
 use File::Spec::Functions qw(catdir catfile rootdir);
 use IO::File qw();
 use Module::Build '0.36' => qw();
+use autodie qw(:all move read_file write_file);
 use parent 'Module::Build';
 use Locale::Maketext::Extract;
 
@@ -26,7 +29,7 @@ sub ACTION_run {
       # scenario: ./Build run bin/scriptname param1 param2
       do($script);
     }
-    else {    
+    else {
       # scenario: ./Build run
       my ($first_script) = ( glob('bin/*'), glob('script/*')); # take the first script in bin or script subdir
       print STDERR "No params given to run action - gonna start: '$first_script'\n";
@@ -37,6 +40,7 @@ sub ACTION_run {
 sub ACTION_build {
     my ($self) = @_;
     #$self->depends_on('messages'); #temporarily disabled by kmx, the new ACTION_messages() needs more testing
+    $self->depends_on('server');
     $self->SUPER::ACTION_build;
     return;
 }
@@ -63,7 +67,7 @@ sub ACTION_messages {
     my $script = catfile(qw(bin frozen-bubble));
 
     return if (-e $pot) && ((-M $pot) < (-M $script)); # frozen-bubble.pot is newer than bin/frozen-bubble
-    
+
     unlink $pot if -f $pot;
     print "Gonna extract all translation strings\n";
     my $ex1 = Locale::Maketext::Extract->new(verbose => 1, warnings  => 0);
@@ -80,6 +84,53 @@ sub ACTION_messages {
         $ex2->write_po($lang);
     }
 
+    return;
+}
+
+sub ACTION_server {
+    my ($self) = @_;
+    my $server_directory = 'server';
+    my $otarget          = 'fb-server';
+
+    # CBuilder doesn't take shell quoting into consideration,
+    # so the -DVERSION macro does not work like in the former Makefile.
+    # Instead, I'll just preprocess the two files with perl.
+    {
+        my $version = $self->dist_version;
+        # perl -pie again has problems with shell quoting for the -e'' part.
+        for my $cfile (
+            map {catfile($server_directory, $_)} qw(fb-server.c net.c)
+        ) {
+            my $csource = read_file($cfile);
+            $csource =~ s{" VERSION "}{$version};
+            write_file($cfile, $csource);
+        }
+    }
+
+    {
+        my $cbuilder = ExtUtils::CBuilder->new;
+        my @ofiles;
+        for my $cfile (qw(fb-server.c log.c tools.c game.c net.c)) {
+            push @ofiles, $cbuilder->compile(
+                source               => catfile($server_directory, $cfile),
+                extra_compiler_flags => [
+                    qw(-g -Wall -Werror -pipe), # verbatim from Makefile
+                    '-I' . $server_directory, # does not seem to be necessary
+                    $cbuilder->split_like_shell(`pkg-config glib-2.0 --cflags`),
+                    $cbuilder->split_like_shell(`pkg-config glib-2.0 --libs`),
+                ],
+            );
+        }
+        $cbuilder->link_executable(
+            objects            => \@ofiles,
+            exe_file           => catfile($server_directory, $otarget),
+            extra_linker_flags => [
+                $cbuilder->split_like_shell(`pkg-config glib-2.0 --libs`),
+            ],
+        );
+    }
+
+    move(catfile($server_directory, $otarget), 'bin');
     return;
 }
 
