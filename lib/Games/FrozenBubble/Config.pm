d0dae3831e20c987b4c5f432b52a67590e3a74f0
Kartik Thakore <thakore.kartik@gmail.com>
2010-06-28 17:59:31 +0000

    using File::Which to find fb-server

--- lib/Games/FrozenBubble/Config.pm
+++ lib/Games/FrozenBubble/Config.pm
@@ -1,9 +1,11 @@
 package Games::FrozenBubble::Config;
 use Config;
 use File::ShareDir qw(dist_dir);
+use Games::FrozenBubble::ConfigData;
+use File::Which;
 use vars qw(@ISA @EXPORT $FPATH $FLPATH);
 @ISA = qw(Exporter);
 @EXPORT = qw($FPATH $FLPATH);
 $FPATH = dist_dir('Games-FrozenBubble');
-$FLPATH = $Config{sitebinexp};
+$FLPATH = File::Which::which('fb-server');
 1;
