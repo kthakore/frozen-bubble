2538934d7cedc2d67b23674582a330aeb601d3f9
Kartik Thakore <thakore.kartik@gmail.com>
2010-06-28 14:14:27 +0000

    Fixed search for fb-server

--- lib/Games/FrozenBubble/Config.pm
+++ lib/Games/FrozenBubble/Config.pm
@@ -1,8 +1,9 @@
 package Games::FrozenBubble::Config;
+use Config;
 use File::ShareDir qw(dist_dir);
 use vars qw(@ISA @EXPORT $FPATH $FLPATH);
 @ISA = qw(Exporter);
 @EXPORT = qw($FPATH $FLPATH);
 $FPATH = dist_dir('Games-FrozenBubble');
-$FLPATH = "/usr/local/lib/frozen-bubble";
+$FLPATH = $Config{sitebinexp};
 1;
